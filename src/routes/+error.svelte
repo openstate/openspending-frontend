41e3cc0fc6aad7d622d355cecffc9e80a08def61
Mark Lindeman <mark@lindeman.it>
2024-04-10 09:25:21 +0000

    Custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.status === 404}
+<h1>Pagina niet gevonden</h1>
+<div class="row">
+  <div class="col col-sm-12 col-lg-9 col-md-12">
+    <p class="lead">
+      Zoals je kunt zien is Open Spending vernieuwt. Het kan daarom zijn dat oude webadressen en/of bookmarks niet meer werken.
+      We raden je aan om via de navigatie in het menu opnieuw  de gevens te vinden die je zoekt en je eventuele bookmarks aan te passen.
+    </p>
+    <p class="lead">
+      Onze excuses voor het ongemak!
+    </p>
+  </div>
+
+</div>
+{:else}
+<h1>Er is een fout opgetreden</h1>
+<p class="lead">Er is helaas iets fout gegaan, probeert u het later nogmaals.</p>
+<div class="alert alert-warning" role="alert">
+  <code>{$page.status}: {$page.error?.message}</code>
+</div>
+{/if}
