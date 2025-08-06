31079fe399e9c2bd9bafb4812a0b72748b0a4274
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-08-06 15:14:24 +0200

    Changes deploy script to use git

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 ssh Neon mkdir /home/projects/openspending/openspending-frontend/.svelte-kit 2>/dev/null
 scp .svelte-kit/tsconfig.json Neon:/home/projects/openspending/openspending-frontend/.svelte-kit
-ssh Neon rm -rf /home/projects/openspending/openspending-frontend/src  && \
-rsync -Cavz --exclude-from=.gitignore --exclude .git .  Neon:/home/projects/openspending/openspending-frontend/ && \
-ssh Neon sudo docker compose --project-directory /home/projects/openspending/openspending-frontend down && \
-ssh Neon sudo docker compose --project-directory /home/projects/openspending/openspending-frontend up -d --build
+ssh Neon /bin/bash << EOF
+cd /home/projects/openspending/openspending-frontend
+git pull
+sudo docker compose down
+sudo docker compose up -d --build
+EOF
