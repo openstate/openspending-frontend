5d63d829baeed0392bc3d02d5a01a9d95c69de30
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2026-03-31 16:46:58 +0200

    Fixes URL for searching

--- src/routes/gegevens/detaildata/redirect-zoekresultaat/[...filters]/+page.ts
+++ src/routes/gegevens/detaildata/redirect-zoekresultaat/[...filters]/+page.ts
@@ -5,6 +5,7 @@ type Resultaat = {
   Title: string
   TitleType: string
   Workspace: string
+  Period: number,
   Code: string
   Slug: string
   Source: string
@@ -17,7 +18,7 @@ export async function load({ params, data }) {
   const session = data.session
   try {
     const resultaat = JSON.parse(params.filters.replace(/^resultaat\//, '')) as Resultaat
-    const url = `/detaildata/redirect-zoekresultaat/Gemeenten/${resultaat.Slug}/${resultaat.Workspace}/${resultaat.TitleType}/${resultaat.Code}`
+    const url = `/detaildata/redirect-zoekresultaat/Gemeenten/${resultaat.Slug}/${resultaat.Period}/${resultaat.TitleType}/${resultaat.Code}`
     const path = await apiGet(url, session.Token)
       .then(async res => {
         if (!res.ok) redirect(302, '/')
